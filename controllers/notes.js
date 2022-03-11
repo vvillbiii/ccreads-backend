8e7f1f3afae954c0dc2840bc737eeaf7f0d8a2f5
vvillbii <will.bowles90@gmail.com>
2022-03-11 16:08:04 -0500

    added update and delete routes for notes

--- controllers/notes.js
+++ controllers/notes.js
@@ -27,13 +27,25 @@ router.get("/", verifyAuth, async (req, res) => {
   }
 });
 
-// router.put("/", verfiyAuth, async (req, res) => {
-//   const { _id } = req.user;
-//   //   try {
-//   //     res.json(await Note.find({ _id }));
-//   //   } catch (error) {
-//   //     res.status(400).json(error);
-//   //   }
-// });
+router.put("/:id", verifyAuth, async (req, res) => {
+  const { _id } = req.user;
+  const id = req.params.id;
+  req.body.user = toId(_id);
+  const body = req.body;
+  try {
+    res.json(await Note.findByIdAndUpdate(id, body, { new: true }));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.delete("/:id", verifyAuth, async (req, res) => {
+  const id = req.params.id;
+  try {
+    res.json(await Note.findByIdAndDelete(id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 module.exports = router;
